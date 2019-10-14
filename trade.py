42f2ba8252a87d74574b17c34175f6d342f4a6ae
Andrew Morse <000morse@gmail.com>
2019-10-14 11:53:22 -0500

    Added purchasing function

--- trade.py
+++ trade.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import os
 
+capital = 10000000 # initialize starting capital
+
 # lists of scores and stock ranks, by sector
 score = []
 stockrank = []
@@ -40,3 +42,46 @@ for file in os.listdir('Sectors-orig/'):
     tmp = tmp.drop(['Unnamed: 0'], axis=1)
     tmp = tmp.drop(['sum'], axis=1)
     prices = prices.join(tmp)
+
+# create list of owned stocks
+ownedstocks = []
+
+# function run daily to buy
+# day = day of analysis, nsectors = number of sectors to buy from, nstocks = number of stocks to buy per sector,
+# tcost = transaction cost, as a percentage above 1
+def buy(day, nsectors=3, nstocks=3, tcost=1.0):
+    global capital
+    bestsectors = []
+    beststocks = []
+    scorelist = []
+    for i in range(nsectors):
+        bestsectors.append(sectorrank[str(i)][day])
+    for i in bestsectors:
+        for j in range(nstocks):
+            beststocks.append([i, stockrank[i][str(j)][day]])
+    for i in beststocks:
+        scorelist.append(score[i[0]][score[i[0]].columns[i[1]]][day])
+
+    j = 0 # initialize counter or number of stocks deleted
+
+    # ensure we only buy stocks with positive score
+    for i in range(len(scorelist)):
+        if scorelist[i-j] <= 0:
+            del scorelist[i-j]
+            del beststocks[i-j]
+            j += 1
+
+    prop = [] # proportion of capital to invest in each stock
+    investval = [] # amount of capital to invest in each stock
+    ninvest = [] # quantity of each stock to buy
+
+    # add stocks to list of owned stocks
+    for i in range(len(scorelist)):
+        prop.append(scorelist[i] / np.sum(scorelist))
+        investval.append(capital*prop[i])
+        ninvest.append(int(np.floor(investval[i] / (tcost * prices[score[beststocks[i][0]].columns[beststocks[i][1]]][day]))))
+
+    # decrement capital after purchase
+    for i in range(len(beststocks)):
+        ownedstocks.append([score[beststocks[i][0]].columns[beststocks[i][1]], ninvest[i]])
+        capital -= ninvest[i] * tcost * prices[score[beststocks[i][0]].columns[beststocks[i][1]]][day]
