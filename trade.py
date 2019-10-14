7f827ef3786145ae5e8a5310681d2d8b189e2f35
Andrew Morse <000morse@gmail.com>
2019-10-14 10:43:57 -0500

    Reading stock prices

--- trade.py
+++ trade.py
@@ -28,3 +28,15 @@ sectorrank = sectorrank.drop(['date'], axis=1)
 # write data frame of sector names
 sectorname = pd.read_csv('Scores/sectornames.csv')
 sectorname = sectorname.drop(['Unnamed: 0'], axis=1) # remove bad column
+
+prices = pd.DataFrame(index=range(0,376))
+prices.set_index(sectorrank.index, inplace=True)
+
+# read all original stock prices
+for file in os.listdir('Sectors-orig/'):
+    tmp = pd.read_csv('Sectors-orig/{}'.format(file))
+    tmp.set_index(pd.to_datetime(tmp.date), inplace=True)
+    tmp = tmp.drop(['date'], axis=1) # drop unneeded cols
+    tmp = tmp.drop(['Unnamed: 0'], axis=1)
+    tmp = tmp.drop(['sum'], axis=1)
+    prices = prices.join(tmp)
